c9573f66af05af039eeaffdbdb82b70df97e1638
Nico B Trivelli <ninotrivelli@gmail.com>
2024-12-01 23:23:14 -0300

    test

--- src/main/java/uy/edu/ort/devops/ordersserviceexample/OrdersServiceExampleApplication.java
+++ src/main/java/uy/edu/ort/devops/ordersserviceexample/OrdersServiceExampleApplication.java
@@ -10,11 +10,14 @@ import uy.edu.ort.devops.ordersserviceexample.logic.OrdersLogic;
 @SpringBootApplication
 public class OrdersServiceExampleApplication {
 
-	public static void main() {
+	public static void main(String[] args) {
+		 // Default values for testing
     String paymentsServiceUrl = "http://payments-service-alb-1611589606.us-east-1.elb.amazonaws.com/payments";
     String shippingServiceUrl = "http://shipping-service-alb-15976227.us-east-1.elb.amazonaws.com/shipping";
     String productsServiceUrl = "http://products-service-alb-110946858.us-east-1.elb.amazonaws.com/products";
 
+
+    // Set the URLs in the OrdersLogic
     OrdersLogic.setPaymentsServiceUrl(paymentsServiceUrl);
     OrdersLogic.setShippingServiceUrl(shippingServiceUrl);
     OrdersLogic.setProductsServiceUrl(productsServiceUrl);
